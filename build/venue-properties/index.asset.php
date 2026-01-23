9c1f95cdce52b80579d98e44cf0191366040ef41
David Marshall <dpegasusm@gmail.com>
2026-01-23 00:28:30 -0500

    Style

--- build/venue-properties/index.asset.php
+++ build/venue-properties/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins'), 'version' => '5c03191b44cb0d79e745');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins'), 'version' => '8285edc9e6b52d8bb839');
